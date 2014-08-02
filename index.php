8a27cf9cd648e4363d1e2f0a1cdd4385ec40f34d
ShinDarth <borzifrancesco@gmail.com>
2014-08-02 11:46:33 +0200

    Loading web base

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>PvPstats</title>
+
+    <link href="css/bootstrap-cyborg.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="visible-xs navbar-brand" href="#">PvPstats</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-center">
+            <li class="active"><a href="#">All levels</a></li>
+            <li><a href="">80</a></li>
+            <li><a href="">70-79</a></li>
+            <li><a href="">60-69</a></li>
+            <li><a href="">50-59</a></li>
+            <li><a href="">40-49</a></li>
+            <li><a href="">30-39</a></li>
+            <li><a href="">20-29</a></li>
+            <li><a href="">10-19</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+    <div class="container main-box">
+
+      <div class="main-title">
+        <h1>PvPstats</h1>
+        <p class="lead">BattleGrounds scores.</p>
+      </div>
+
+      <div class="row">
+        <div class="col-md-3 col-sm-6" style="padding: 0 10px;">
+          <p class="h3 text-center">Today</p>
+          <div class="score-container">
+            <table class="table table-striped text-center">
+              <tr>
+                <th class="text-center">#</th>
+                <th class="text-center">Character</th>
+                <th class="text-center">Victories</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>Namename</td>
+                <td>12</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Namename</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Namename</td>
+                <td>1</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6" style="padding: 0 10px;">
+          <p class="h3 text-center">Last 7 days</p>
+          <div class="score-container">
+            <table class="table table-striped text-center">
+              <tr>
+                <th class="text-center">#</th>
+                <th class="text-center">Character</th>
+                <th class="text-center">Victories</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>Namename</td>
+                <td>12</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Namename</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Namename</td>
+                <td>1</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6" style="padding: 0 10px;">
+          <p class="h3 text-center">This month</p>
+          <div class="score-container">
+            <table class="table table-striped text-center">
+              <tr>
+                <th class="text-center">#</th>
+                <th class="text-center">Character</th>
+                <th class="text-center">Victories</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>Namename</td>
+                <td>12</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Namename</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Namename</td>
+                <td>1</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-6" style="padding: 0 10px;">
+          <p class="h3 text-center">Overall</p>
+          <div class="score-container">
+            <table class="table table-striped text-center">
+              <tr>
+                <th class="text-center">#</th>
+                <th class="text-center">Character</th>
+                <th class="text-center">Victories</th>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td>Namename</td>
+                <td>12</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Namename</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>Namename</td>
+                <td>1</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
