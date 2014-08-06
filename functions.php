4bdafa8c92f5d8b061e0446d373e615b4244bc42
ShinDarth <borzifrancesco@gmail.com>
2014-08-06 15:20:50 +0200

    move functions implementation in functions.php file

---
+++ functions.php
@@ -0,0 +1,92 @@
+<?php
+
+function getPlayerName($guid)
+{
+  global $db;
+
+  $query = sprintf("SELECT name FROM characters WHERE guid = %d;", $guid);
+  $row = $db->query($query)->fetch_array();
+
+  return $row['name'];
+}
+
+function getFactionScores($time_cond, $level_cond)
+{
+  global $db, $ALLIANCE, $HORDE;
+
+  $score[2];
+
+  if ($time_cond != "")
+    $time_cond = "AND " . $time_cond;
+
+  if ($level_cond != "")
+    $level_cond = "AND " . $level_cond;
+
+  $query = sprintf("SELECT COUNT(*) FROM pvpstats_faction WHERE faction = %d %s %s UNION SELECT COUNT(*) FROM pvpstats_faction WHERE faction = %d %s %s;",
+                   $ALLIANCE, $time_cond, $level_cond, $HORDE, $time_cond, $level_cond);
+
+  $result = $db->query($query);
+
+  $row = $result->fetch_row();
+  $score[0] = $row[0];
+
+  $row = $result->fetch_row();
+
+  if ($row != null)
+    $score[1] = $row[0];
+  else
+    $score[1] = 0;
+
+  return $score;
+}
+
+function getPlayersScores($time_cond, $level_cond)
+{
+  global $db, $limit, $players_group_and_order;
+
+
+  if ($time_cond == "" && $level_cond == "")
+    $where = "";
+  else
+    $where = "WHERE";
+
+  if ($time_cond != "" && $level_cond != "")
+    $level_cond = "AND " . $level_cond;
+
+  $query = sprintf("SELECT character_guid, count(character_guid) FROM pvpstats_players %s %s %s %s %s",
+                   $where,
+                   $time_cond,
+                   $level_cond,
+                   $players_group_and_order,
+                   $limit);
+
+  $result = $db->query($query);
+
+  $row = $result->fetch_row();
+
+  if ($row == null)
+    return;
+
+  $position = 1;
+
+  printf("<tr><td>%d</td><td>%s</td><td>%d</td></tr>",
+         $position,
+         getPlayerName($row[0]),
+         $row[1]);
+
+  $prev_score = $row[1];
+
+
+  while (($row = $result->fetch_row()) != null)
+  {
+    if ($prev_score != $row[1])
+      $position++;
+
+    printf("<tr><td>%d</td><td>%s</td><td>%d</td></tr>",
+           $position,
+           getPlayerName($row[0]),
+           $row[1]);
+
+    $prev_score = $row[1];
+  }
+}?>
--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 
   require_once("config.php");
 
-
   // query variables
 
   $ALLIANCE = 469;
   $HORDE = 67;
   $limit = "LIMIT 0,20";
   $players_group_and_order = "GROUP BY character_guid ORDER BY count(character_guid) DESC";
-
 
   // query conditions
 
@@ -28,100 +26,9 @@
     $level = "all";
   }
 
-
   // query functions
 
-  function getPlayerName($guid)
-  {
-    global $db;
-
-    $query = sprintf("SELECT name FROM characters WHERE guid = %d;", $guid);
-    $row = $db->query($query)->fetch_array();
-
-    return $row['name'];
-  }
-
-  function getFactionScores($time_cond, $level_cond)
-  {
-    global $db, $ALLIANCE, $HORDE;
-
-    $score[2];
-
-    if ($time_cond != "")
-      $time_cond = "AND " . $time_cond;
-
-    if ($level_cond != "")
-      $level_cond = "AND " . $level_cond;
-
-    $query = sprintf("SELECT COUNT(*) FROM pvpstats_faction WHERE faction = %d %s %s UNION SELECT COUNT(*) FROM pvpstats_faction WHERE faction = %d %s %s;",
-                     $ALLIANCE, $time_cond, $level_cond, $HORDE, $time_cond, $level_cond);
-
-    $result = $db->query($query);
-
-    $row = $result->fetch_row();
-    $score[0] = $row[0];
-
-    $row = $result->fetch_row();
-
-    if ($row != null)
-      $score[1] = $row[0];
-    else
-      $score[1] = 0;
-
-    return $score;
-  }
-
-  function getPlayersScores($time_cond, $level_cond)
-  {
-    global $db, $limit, $players_group_and_order;
-
-
-    if ($time_cond == "" && $level_cond == "")
-      $where = "";
-    else
-      $where = "WHERE";
-
-    if ($time_cond != "" && $level_cond != "")
-      $level_cond = "AND " . $level_cond;
-
-    $query = sprintf("SELECT character_guid, count(character_guid) FROM pvpstats_players %s %s %s %s %s",
-                     $where,
-                     $time_cond,
-                     $level_cond,
-                     $players_group_and_order,
-                     $limit);
-
-    $result = $db->query($query);
-
-    $row = $result->fetch_row();
-
-    if ($row == null)
-      return;
-
-    $position = 1;
-
-    printf("<tr><td>%d</td><td>%s</td><td>%d</td></tr>",
-          $position,
-          getPlayerName($row[0]),
-          $row[1]);
-
-    $prev_score = $row[1];
-
-
-    while (($row = $result->fetch_row()) != null)
-    {
-      if ($prev_score != $row[1])
-        $position++;
-
-      printf("<tr><td>%d</td><td>%s</td><td>%d</td></tr>",
-            $position,
-            getPlayerName($row[0]),
-            $row[1]);
-
-      $prev_score = $row[1];
-    }
-  }
-
+  require_once("functions.php");
 
   // faction scores
 
