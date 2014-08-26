1c63bffc9e60a23fde9db63c6438a8bb4cc7e5db
ShinDarth <borzifrancesco@gmail.com>
2014-08-26 19:20:51 +0200

    DROP TABLE IF EXISTS before CREATE it

--- install/characters_pvpstats.sql
+++ install/characters_pvpstats.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS `pvpstats_faction`;
 CREATE TABLE `pvpstats_faction` (
   `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
   `faction` BIT NOT NULL,
@@ -6,6 +7,7 @@ CREATE TABLE `pvpstats_faction` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
+DROP TABLE IF EXISTS `pvpstats_players`;
 CREATE TABLE `pvpstats_players` (
   `character_guid` INT UNSIGNED NOT NULL,
   `level` TINYINT UNSIGNED NOT NULL,
