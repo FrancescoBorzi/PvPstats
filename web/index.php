f76b5bfbfa439477c58b4b2e33ad30cd408708e9
ShinDarth <borzifrancesco@gmail.com>
2014-09-05 17:18:07 +0200

    meta values

--- web/index.php
+++ web/index.php
@@ -13,9 +13,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
+    <meta name="description" content="PvPstats, see who is winning!">
+    <meta name="author" content="ShinDarth">
 
     <title><?= $server_name ?> PvPstats</title>
 
